13ed7b7a738b7883dd4feb44928776b7e5ff47c7
kevin berg <kevinberg@live.se>
2023-11-24 09:28:25 +0100

    Featuredproduct och showcase 2 klar

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ function App() {
         <Route path='loginview' element={<Login2/>}/>
         <Route path='/PaymentMethod' element={<PaymentMethod/>}/>
         <Route path='/AddANewCard' element={<AddANewCard/>} />
+        
       </Routes>
     </BrowserRouter>
   )
--- src/views/Components/Kevin/Showcase2.jsx
+++ src/views/Components/Kevin/Showcase2.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
-const Showcase = () => {
+const Showcase2 = () => {
   return (
     <section className="showcase">
     <div className="container">
       <div className="column-1">
         <div className="content">
-          <div className="title-1">Welcome to Manero!</div>
-          <div className="title-2">50% off</div>
+          <div className="title-3">Take 50% off now!</div>
+          <div className='showcase-button'>
+          <button className='product-button'> Quick view </button>
+
+          </div>
         </div>
       </div>
       <div className="column-2">
@@ -18,4 +21,4 @@ const Showcase = () => {
   );
 }
 
-export default Showcase;
+export default Showcase2;
