63c5c6083faf1f2c882436c54ae5e136d30c5fd9
kevin berg <kevinberg@live.se>
2023-11-28 13:25:48 +0100

    view all sidan klar för featured och små fix

--- src/views/Components/Kevin/FeaturedProducts.jsx
+++ src/views/Components/Kevin/FeaturedProducts.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faStar as solidStar, faStar as regularStar } from '@fortawesome/free-solid-svg-icons';
 
@@ -28,7 +30,7 @@ const FeaturedProducts = () => {
     async function getProducts(tag) {
       try{
         const token = localStorage.getItem("token"); 
-        const res = await fetch(`https://localhost:7056/api/${tag}`,{
+        const res = await fetch(`https://localhost:7056/api/products/take?take=4&category=Featured`,{
           headers: {
             'Authorization': `Bearer ${token}`, 
           },
@@ -52,40 +54,54 @@ const FeaturedProducts = () => {
 
   return (
     <div>
-      
-    <section className="product-collection">
+  <section className="product-collection">
     <div className="container">
       <div className='Featured-header'>
         <div className="title">Featured Products</div>
-        <a className='View-All'>view all <i class="fa-solid fa-chevron-right"></i> </a>
-
+        <Link to='/ViewAllFeatrued' className='View-All'>
+        view all <i className="fa-solid fa-chevron-right"></i>
+      </Link>
       </div>
       <div className="collection-grid">
-      {products.map(item => (
-  <div className="collection-card" key={item.articleNumber}>
-    <div className="card-content">
-      <img src={item.imageLink} alt={item.name} /> 
-      <div className="card-menu">
-        <nav className="icons">
-          <a className="link" href="#"><i className="fa-regular fa-heart"></i></a>
-          <a className="link" href="#"><i className="fa-regular fa-bag-shopping"></i></a>
-        </nav>
-      </div>
-    </div>
-    <div className="card-body">
-      <p className="title">{item.color} {item.name} </p>
-      <div className="ranking">
-       
-      </div>
-      <p className="price">{item.price} kr</p>
-    </div>
-  </div>
-))}
+        {products.map(item => (
+          item.category === "Featured" && (
+            <div className="collection-card" key={item.articleNumber}>
+              <div className="card-content">
+                <img src={item.imageLink} alt={item.name} />
+                <div className="card-menu">
+                  <nav className="icons">
+                    <a className="link" href="#"><i className="fa-regular fa-heart"></i></a>
+                    <a className="link" href="#"><i className="fa-regular fa-bag-shopping"></i></a>
+                  </nav>
+                </div>
+              </div>
+              <div className="card-body">
+                <p className="title">{item.name}</p>
+                <p className="color">{item.color[0].color}</p>
+                <div className="ranking">
+
+                </div>
+                <p className="price">{item.price} kr</p>
+              </div>
+            </div>
+          )
+        ))}
       </div>
     </div>
   </section>
-    </div>
+</div>
+
+  
 );
 }
 
 export default FeaturedProducts;
+
+
+
+ {/* {item.color.map(colorItem => (
+                    <p key={colorItem.colorId} className="title">{colorItem.color} {item.name} </p>
+                  ))}
+                  {item.size.map(sizeItem => (
+                    <p key={sizeItem.sizeId} className="title">{sizeItem.size} {item.name} </p>
+                  ))} */}
